243957429bba817f30322d58e085e596aee67c64
Anna Pittman <annaepittman@gmail.com>
2021-10-22 10:52:14 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-  $("formOne").submit(function(event) {
+  $("#formOne").submit(function(event) {
     event.preventDefault();
     const question1 = $("#question1").val();
     const question2 = $("question2").val();
     const question3 = $("question3").val();
-    const question4 = $("question4").val();
-    const question5 = $("question5").val();
+    // const question4 = $("question4").val();
+    // const question5 = $("question5").val();
     
     let result;
   
-    if (question1 === "Green" && (question2 === "Summer" || question3 ==="Working at a bigger, well-known company")) {
+    if (question1 === "Green" && (question2 ==="Summer" || question3 ==="Working at a bigger, well-known company")) {
     result = "C#";
   }
     else if (question1 === "Red" && (question2 === "Winter" || question3 === "Working at a smaller startup")) {
@@ -21,7 +21,7 @@ $(document).ready(function() {
     }
 
     else {
-      result = "JavaScript";
+      result = "Any language!"
     }
 
     $("#resultLang").text(result);
