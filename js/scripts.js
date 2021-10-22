292850c2fd47815850736cdc84131f1866910321
Anna Pittman <annaepittman@gmail.com>
2021-10-22 09:33:03 -0700

    add questions four and five to html file, add function and variables to JavaScript file

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     const question1 = $("#question1").val();
     const question2 = $("question2").val();
     const question3 = $("question3").val();
+    const question4 = $("question4").val();
+    const question5 = $("question5").val();
   
   });
 });
