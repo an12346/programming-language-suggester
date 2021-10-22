1c71ab6fffb324a01bf541bd31a99b875e8f3ed3
Anna Pittman <annaepittman@gmail.com>
2021-10-22 11:29:08 -0700

    edit questions and else, if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+  $("form#formLang").submit(function(event) {
     event.preventDefault();
     const question1 = $("#question1").val();
-    const question2 = $("question2").val();
-    const question3 = $("question3").val();
-    // const question4 = $("question4").val();
-    // const question5 = $("question5").val();
+    const question2 = $("#question2").val();
+    const question3 = $("#question3").val();
+    const question4 = $("question4").val();
+    const question5 = $("question5").val();
     
     let result;
   
-    if (question1 === "Green" && (question2 ==="Summer" || question3 ==="Working at a bigger, well-known company")) {
-    result = "C#";
+    if (question1 ==="Green" && (question2 ==="Video Games" || question3 ==="Working at a bigger, well known company")) {
+    result = "Learn C# !";
   }
-    else if (question1 === "Red" && (question2 === "Winter" || question3 === "Working at a smaller startup")) {
-      result = "Ruby";
+    else if (question1 ==="Red" && (question2 ==="E-commerce websites" || question3 ==="Working at a smaller startup")) {
+      result = "Learn Ruby !";
     }
 
-    else if (question1 === "Blue" && (question2 == "Other" || question3 === "Either/Both")) {
-      result = "JavaScript";
+    else if (question1 ==="Blue" && (question2 =="Interactive web-pages" || question3 ==="Either/Both")) {
+    result = "JavaScript";
     }
 
     else {
-      result = "Any language!"
+      result = "Any language!";
     }
 
     $("#resultLang").text(result);
