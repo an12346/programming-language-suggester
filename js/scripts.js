408646ad03b6e8ded6bbfca2456c32efe6072a22
Anna Pittman <annaepittman@gmail.com>
2021-10-22 08:57:39 -0700

    add question two and three to html file, change questions into select boxes format

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("formOne").submit(function(event) {
+    event.preventDefault();
+    const question1 = $("#question1").val();
+    const question2 = $("question2").val();
+    const question3 = $("question3").val();
+  
+  });
+});
