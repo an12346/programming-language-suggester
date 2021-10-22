9fae6097f1d48451c1999cdd3d8f9a027ba43ee6
Anna Pittman <annaepittman@gmail.com>
2021-10-22 10:20:56 -0700

    add branching statements to js file

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,27 @@ $(document).ready(function() {
     const question3 = $("question3").val();
     const question4 = $("question4").val();
     const question5 = $("question5").val();
+    
+    let result;
+  
+    if (question1 === "Green" && (question2 === "Summer" || question3 ==="Working at a bigger, well-known company")) {
+    result = "C#";
+  }
+    else if (question1 === "Red" && (question2 === "Winter" || question3 === "Working at a smaller startup")) {
+      result = "Ruby";
+    }
+
+    else if (question1 === "Blue" && (question2 == "Other" || question3 === "Either/Both")) {
+      result = "JavaScript";
+    }
+
+    else {
+      result = "JavaScript";
+    }
+
+    $("#resultLang").text(result);
+    $("#resultLang").show();
+  
   
   });
 });
